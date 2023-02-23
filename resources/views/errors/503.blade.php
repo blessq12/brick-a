f064ce7c5fe54e9b1289e09d00b0aaa23d6d1a67
Andrew <code70@inbox.ru>
2023-02-23 19:15:05 +0700

    init

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="robots" content="noindex,nofollow">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Брик-А - Торгово строительная компания.</title>
+    <meta name="description" content="На сайте ведутся технические работы, приносим извинения за неудобства."> 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"> 
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-10 col-lg-10">
+                <h1>Режим отладки</h1>
+                <p>Сайт временно недоступен из-за технических работ.</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
