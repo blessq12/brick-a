413c4959f56733b8f31e7b46d58a1e3750116303
Andrew <code70@inbox.ru>
2023-02-26 22:07:07 +0700

    dev page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -10,12 +10,10 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"> 
 </head>
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-12 col-md-10 col-lg-10">
-                <h1>Режим отладки</h1>
-                <p>Сайт временно недоступен из-за технических работ.</p>
-                <a href="">кнопка</a>
+    <div class="container py-5">
+        <div class="row justify-content-center align-items-center" style="min-height:80vh;">   
+            <div class="col-12 col-md-8 col-lg-6">
+                <img src="/assets/svg/named_logo.svg" alt="" class="mb-4">
             </div>
         </div>
     </div>
