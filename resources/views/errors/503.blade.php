080a4408dcd591549483b2c1b3a268bd2d4a4584
Andrew <code70@inbox.ru>
2023-02-26 22:39:28 +0700

    robots,description

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -3,8 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex,nofollow">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Технические работы</title>
+    <meta name="description" content="Мы работаем над улучшением нашего сайта и совсем скоро он будет доступен.">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
     <style>
         body{
