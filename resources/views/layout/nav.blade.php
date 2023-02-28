cd9404b5667ebc9c34f5927c0008257c135f4c64
Andrew <code70@inbox.ru>
2023-02-28 16:12:16 +0700

    images and styles

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -9,33 +9,9 @@
             </div>
             <div class="collapse navbar-collapse" id="navbar-menu">
                 <ul class="nav navbar-nav navbar-right" data-in="fadeInDown" data-out="fadeOutUp">
-                    <li class="dropdown">
-                        <a href="#home" class="dropdown-toggle smooth-menu" data-toggle="dropdown" >Home</a>
-                        <ul class="dropdown-menu">
-                            <li><a href="index.html">Home Version 1</a></li>
-                            <li><a href="index-2.html">Home Version 2</a></li>
-                            <li><a href="index-3.html">Home Version 3</a></li>
-                            <li><a href="index-4.html">Home Version 4</a></li>
-                            <li><a href="index-5.html">Home Version 5</a></li>
-                        </ul>
-                    </li>
+                    
                     <li>
-                        <a class="smooth-menu" href="#about">About</a>
-                    </li>
-                    <li>
-                        <a class="smooth-menu" href="#services">Services</a>
-                    </li>
-                    <li>
-                        <a class="smooth-menu" href="#portfolio">Portfolio</a>
-                    </li>
-                    <li>
-                        <a class="smooth-menu" href="#pricing">Pricing</a>
-                    </li>
-                    <li>
-                        <a class="smooth-menu" href="#blog">blog</a>
-                    </li>
-                    <li>
-                        <a class="smooth-menu" href="#contact">contact</a>
+                        <a href="/slug">About</a>
                     </li>
                 </ul>
             </div>
